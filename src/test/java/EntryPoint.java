a284e241b17c9dff056d3152d90d42de725083dd
Akhmadali Azizov <ahmadali.azizov@outlook.com>
2019-10-21 21:06:00 -0500

    HW/class lesson

---
+++ src/test/java/EntryPoint.java
@@ -0,0 +1,11 @@
+public class EntryPoint {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+        myCat.name = "Gav";
+        myCat.voice = "Puss";
+        Cat myCat2 = new Cat();
+        myCat2.name = "Gav2";
+        myCat2.voice = "Puss2";
+        System.out.println("");
+    }
+}
