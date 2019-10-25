c5f586b2132f2883f8090f9cd77c36b68a956495
Akhmadali Azizov <ahmadali.azizov@outlook.com>
2019-10-24 23:48:00 -0500

    Primitive Data Types homework

---
+++ src/main/java/Panda.java
@@ -0,0 +1,32 @@
+public class Panda {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private double weight;
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
+
--- src/test/java/Cat.java
+++ src/test/java/Cat.java
@@ -1,4 +1,12 @@
 public class Cat {
     String name;
     String voice;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
