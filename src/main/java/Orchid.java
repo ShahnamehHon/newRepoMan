508675af5bafaa843f5bd6afecde2553f34e55ff
Akhmadali Azizov <ahmadali.azizov@outlook.com>
2019-10-30 16:20:42 -0500

    ver 1.1 HW day6

--- src/main/java/Orchid.java
+++ src/main/java/Orchid.java
@@ -21,6 +21,31 @@ public class Orchid {
         this.cutOrchid = cutOrchid;
         this.species = species;
     }
+    public Orchid(int cutOrchid) {
+        this.cutOrchid = cutOrchid;
+    }
+    public Orchid(int cutOrchid, String name) {
+        this.cutOrchid = cutOrchid;
+        this.name = name;
+    }
+
+    public Orchid(int cutOrchid, String name, double species) {
+        this.cutOrchid = cutOrchid;
+        this.name = name;
+        this.species = species;
+    }
+    public Orchid(double species) {
+        this.species = species;
+    }
+    public Orchid(double species, String name) {
+        this.species = species;
+        this.name = name;
+    }
+    public Orchid(double species, String name, int cutOrchid) {
+        this.species = species;
+        this.name = name;
+        this.cutOrchid = cutOrchid;
+    }
 
     public String getName() {
         return name;
